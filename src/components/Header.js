1b15cb092c559a4715a52744716d3bf9d6f8a66d
Glitch (starter-react-template) <none>
2020-05-23 02:42:46 +0000

    🌸🎐 Checkpoint ./src/components/Header.js:15532837/440 ./src/App.js:15532837/2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import 
 
 class Header extends Component {
     render() {
         return <div class="header">
-                  
+                  <div class="header-left">
+                    <p>Go back</p>
+                  </div> 
+                  <div class="header-center">
+                    <p>CodersX</p>
+                  </div>
+                  <div class="header-right">
+                    <button>Sign up</button>
+                  </div> 
                </div>;
     }
 }
