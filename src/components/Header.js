2c4ba0e3cea8352216c1f82f34b08d1280f061d6
Glitch (starter-react-template) <none>
2020-05-23 02:18:37 +0000

    ⛰🚠 Checkpoint ./src/components/Header.js:15532837/237

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return <div>
+                <p>Đi chợ</p>
+            </div>;
+    }
+}
+
+export default Header;
