6b4b798ec9db8b699694c0eedb49b1336600998f
Glitch (starter-react-template) <none>
2020-05-23 10:51:46 +0000

    👟😻 Checkpoint ./src/App.js:15532837/45 ./src/components/Header.js:15532837/1392 ./src/styles/Header.css:15532837/300

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,25 @@ import '../styles/Header.css'
 
 class Header extends Component {
     render() {
-        return <div class="header">
-                  <div class="header-left">
-                    <p>Go back</p>
+        return <div class="recommended-friends">
+                  <div class="recommended-friends-header">
+                    <div class="recommended-friends-header-left">
+                      <p>Gợi ý cho bạn</p>
+                    </div>
+                    <div class="recommended-friends-header-right">
+                      <p>Xem tất cả</p>
+                    </div> 
                   </div> 
-                  <div class="header-center">
-                    <p>CodersX</p>
-                  </div>
-                  <div class="header-right">
-                    <button>Sign up</button>
+                  <div class="recommended-friends-content">
+                    <div class="recommended-friends-content-left">
+                      <p>Gợi ý cho bạn</p>
+                    </div>
+                    <div class="recommended-friends-content-center">
+                      <p>Gợi ý cho bạn</p>
+                    </div>
+                    <div class="recommended-friends-content-right">
+                      <p>Xem tất cả</p>
+                    </div> 
                   </div> 
                </div>;
     }
